a2d7fa1e8f3d5249a6d8068a0d6f00fcba0827f7
Yujeong Christina Yun <yjc.yun@gmail.com>
2020-05-16 22:16:32 -0400

    error page button navigate to home and gh-page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ErrorImg from '../image/error.png';
 import Hero from '../components/Home/Hero';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
     <Hero
       img={ErrorImg}
       title="404"
-      title3="Oops!" 
+      title3="Oops!"
       title4="This page does not exist">
-      <button className="btn left-btn" style={{marginTop:'3rem'}}>return home</button>
-      </Hero>
+      <Link to="/">
+        <button className="btn left-btn" style={{ marginTop: '3rem' }}>return home</button>
+      </Link>
+    </Hero>
   )
 }
 
