dc622cce1bf860710347c1a2dc48f1091b306ad2
Yujeong Christina Yun <yjc.yun@gmail.com>
2020-05-28 17:27:53 -0400

    Updates

---
+++ precache-manifest.cee88e4e6fd80c9232bc235dd123a736.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d2c812dce587a01c00fe9797b59a60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2931b1863b602141becf",
+    "url": "/static/css/main.054e6d9b.chunk.css"
+  },
+  {
+    "revision": "cb708ae0d1e39c1d34cc",
+    "url": "/static/js/2.4cd6cce8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4cd6cce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2931b1863b602141becf",
+    "url": "/static/js/main.5a07927d.chunk.js"
+  },
+  {
+    "revision": "7dc45054942f0ee89185",
+    "url": "/static/js/runtime-main.75293954.js"
+  },
+  {
+    "revision": "df6f4e493ed7f7d25a9c0a4e69dedc78",
+    "url": "/static/media/adopt-1.df6f4e49.png"
+  },
+  {
+    "revision": "bbcbef337256e1d0efc6f1c3a6bc44d0",
+    "url": "/static/media/dognews.bbcbef33.png"
+  },
+  {
+    "revision": "e971d617b1fa82f2bb55f72ea92d0959",
+    "url": "/static/media/error.e971d617.png"
+  },
+  {
+    "revision": "05b95c22be74df4b712dca2d6d1185b6",
+    "url": "/static/media/fabric-1.05b95c22.png"
+  },
+  {
+    "revision": "1943024b52f28121b09d5595d34f8166",
+    "url": "/static/media/mainBcg.1943024b.png"
+  },
+  {
+    "revision": "e0ecd5c7575d47eb4b8025240437388c",
+    "url": "/static/media/personalwebsite.e0ecd5c7.png"
+  },
+  {
+    "revision": "3cb1c60daec08a34f66e6e2422cbc72a",
+    "url": "/static/media/plant-1.3cb1c60d.png"
+  }
+]);
